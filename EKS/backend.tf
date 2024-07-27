1619508febe365fea98fa8a32582884eaceb159b
prashantpartake <43843844+partake93@users.noreply.github.com>
2024-07-28 03:47:35 +0800

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks"
+    bucket = "cicdjenkin"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-southeast-1"
   }
 }
